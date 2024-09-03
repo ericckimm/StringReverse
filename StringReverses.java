ef46a966c0666e8be0b0d90869e2bff542802e62
Eric Kim <kim.ericdh@gmail.com>
2024-09-03 13:26:37 -0600

    :)))

--- StringReverses.java
+++ StringReverses.java
@@ -3,20 +3,28 @@ public class StringReverses
     public static void main (String[] args)
     {
         char[] word = {'h', 'e', 'l', 'l', 'o'};
-        char[] reversed = reverse(word);
-        for(int i = 0; i < reversed.length; i++)
+        reverse(word);
+        for(int i = 0; i < word.length; i++)
         {
-            System.out.print(reversed[i] + " ");
+            System.out.print(word[i] + " ");
         }
     }
 
-    public static char[] reverse(char[] letters)
+    public static void reverse(char[] letters)
     {
-        char[] reversed = new char[letters.length];
-        for(int i = 0; i < letters.length; i++)
+        int left = 0;
+        int right = letters.length - 1;
+        
+        while (left < right)
         {
-            reversed[i] = letters[letters.length - 1 - i];
+            // Swap the elements at left and right
+            char temp = letters[left];
+            letters[left] = letters[right];
+            letters[right] = temp;
+            
+            // Move the pointers towards the center
+            left++;
+            right--;
         }
-        return reversed;
     }
 }
